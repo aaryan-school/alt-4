56293816219ce03a3c824fdf1d8bfc91693a0b42
arvind-olag <163285299+arvind-olag@users.noreply.github.com>
2024-03-15 11:24:24 +0000

    Timer

--- main.ts
+++ main.ts
@@ -1,4 +1,20 @@
-basic.showIcon(IconNames.Heart)
+let xcount = 0
+let ycount = 0
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
 basic.forever(function () {
-	
+    for (let index = 0; index < 5; index++) {
+        for (let index = 0; index < 5; index++) {
+            basic.pause(1000)
+            led.unplot(xcount, ycount)
+            xcount += 1
+        }
+        ycount += 1
+        xcount = 0
+    }
 })
