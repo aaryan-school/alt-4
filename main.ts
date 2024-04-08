64af7f60f52b9686c7db75d71bccf74fdc096b16
ssnitixx <83287679+aaryan-school@users.noreply.github.com>
2024-04-08 11:40:54 +0100

    Update pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,20 +1,41 @@
+input.onButtonPressed(Button.A, function () {
+    repcount += -1
+})
+input.onButtonPressed(Button.AB, function () {
+    repset = 1
+    basic.showIcon(IconNames.Yes)
+    basic.pause(500)
+})
+input.onButtonPressed(Button.B, function () {
+    repcount += 1
+})
+let repset = 0
+let repcount = 0
+repset = 0
+let count = 3
+let runonce = 0
+let finished = 0
 let xcount = 0
 let ycount = 0
-basic.showLeds(`
-    # # # # #
-    # # # # #
-    # # # # #
-    # # # # #
-    # # # # #
-    `)
 basic.forever(function () {
-    for (let index = 0; index < 5; index++) {
-        for (let index = 0; index < 5; index++) {
-            basic.pause(1000)
-            led.unplot(xcount, ycount)
+    while (repset != 1) {
+        basic.showNumber(repcount)
+    }
+    runonce = 1
+    while (count != 0) {
+        basic.showNumber(count)
+        basic.pause(1000)
+        count += -1
+    }
+    basic.clearScreen()
+    while (finished == 0) {
+        for (let index = 0; index <= repcount; index++) {
+            led.plot(xcount, ycount)
             xcount += 1
+            if (xcount == 4) {
+                ycount += -1
+                xcount = 0
+            }
         }
-        ycount += 1
-        xcount = 0
     }
 })
